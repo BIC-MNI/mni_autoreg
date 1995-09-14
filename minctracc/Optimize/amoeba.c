27064f951e5f2b845d29b0e157f1a5d36d87d499
louis <louis>
1995-09-14 09:56:02 +0000

    removed the call to numerically_close and replaced it with the same terminating condition as used in the previous version of amoeba, so that the same termination tolerance can be used.
    Also removed the warning message "No improvement after X steps..."

--- minctracc/Optimize/amoeba.c
+++ minctracc/Optimize/amoeba.c
@@ -17,18 +17,63 @@
 
 @CREATED    : 
 @MODIFIED   : $Log: amoeba.c,v $
-@MODIFIED   : Revision 1.1  1995-09-07 10:05:11  louis
-@MODIFIED   : Initial revision
+@MODIFIED   : Revision 1.2  1995-09-14 09:56:02  louis
+@MODIFIED   : removed the call to numerically_close and replaced it with the same
+@MODIFIED   : terminating condition as used in the previous version of amoeba, so
+@MODIFIED   : that the same termination tolerance can be used.
 @MODIFIED   :
+@MODIFIED   : Also removed the warning message "No improvement after X steps..."
+@MODIFIED   :
+ * Revision 1.1  1995/09/07  10:05:11  louis
+ * Initial revision
+ *
 
    LC: this version of David's ameoba.c  is slightly modified from that
    found in BIC_PL, in that the function numerically_close is included
-   here, and bic_pl is not needed for minctracc.
+   here, and bic_pl is not needed for minctracc.   This way, only 
+   volume_io.h and amoeba.h are needed for inclusion.
+
+   there are four public functions that the user has to call (in order):
+   initialize_amoeba(), 
+   perform_amoeba(),
+   get_amoeba_parameters(),
+   terminate_amoeba()
+
+
+   1: initialize_amoeba is called first to set up the simplex optimization
+      process.  It requires the follwing parameters:
+   initialize_amoeba(
+      amoeba_struct     *amoeba,                 the structure, allocated by
+                                                 the calling program
+      int               n_parameters,            the number of free parameter
+                                                 to be optimized 
+      Real              initial_parameters[],    the array containing the
+                                                 starting guess
+      Real              parameter_delta,         the radius of the simplex
+      amoeba_function   function,                the name of the objective
+                                                 function
+      void              *function_data,          pointer to any data you
+                                                 want to pass to the objective
+						 function
+      Real              tolerance )              the stopping tolerance
+    
+   2: perform_amoeba(amoeba) is then called to actually do the optimization.
+      The function returns TRUE if the optimization is successful, otherwise
+      it returns FALSE.
+
+   3: get_amoeba_parameters(amoeba, parameters) must be called to extract
+      the optimized parameters.  'parameters' is a Real array, allocated by
+      the calling program.
+
+   4: terminate_amoeba(amoeba) is called in the end to free up internal data
+      structures used by the procedure.  This procedure should be called after
+      get_amoeba_parameters, since the optimized parameters will be freed (and
+      lost) in the call to terminate_amoeba().
 
 ---------------------------------------------------------------------------- */
 
 #ifndef lint
-static char rcsid[]="$Header: /private-cvsroot/registration/mni_autoreg/minctracc/Optimize/amoeba.c,v 1.1 1995-09-07 10:05:11 louis Exp $";
+static char rcsid[]="$Header: /private-cvsroot/registration/mni_autoreg/minctracc/Optimize/amoeba.c,v 1.2 1995-09-14 09:56:02 louis Exp $";
 #endif
 
 
@@ -82,6 +127,20 @@ private  BOOLEAN  numerically_close(
 }
 
 
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_function_value
+@INPUT      : amoeba
+              parameters
+@OUTPUT     : 
+@RETURNS    : function value
+@DESCRIPTION: Evaluates the function being minimized by amoeba, by calling
+              the user function.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :         1993    David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
 private  Real  get_function_value(
     amoeba_struct  *amoeba,
     float          parameters[] )
@@ -89,6 +148,23 @@ private  Real  get_function_value(
     return( (*amoeba->function) ( amoeba->function_data, parameters ) );
 }
 
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : initialize_amoeba
+@INPUT      : n_parameters
+              initial_parameters
+              parameter_deltas
+              function
+              function_data
+              tolerance
+@OUTPUT     : amoeba
+@RETURNS    : 
+@DESCRIPTION: Initializes the amoeba structure to minimize the function.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :         1993    David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
 public  void  initialize_amoeba(
     amoeba_struct     *amoeba,
     int               n_parameters,
@@ -127,6 +203,19 @@ public  void  initialize_amoeba(
     }
 }
 
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_amoeba_parameters
+@INPUT      : amoeba
+@OUTPUT     : parameters
+@RETURNS    : function value
+@DESCRIPTION: Passes back the current position of the amoeba (best value),
+              and returns the function value at that point.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :         1993    David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
 public  Real  get_amoeba_parameters(
     amoeba_struct  *amoeba,
     Real           parameters[] )
@@ -146,6 +235,19 @@ public  Real  get_amoeba_parameters(
     return( amoeba->values[low] );
 }
 
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : terminate_amoeba
+@INPUT      : amoeba
+@OUTPUT     : 
+@RETURNS    : 
+@DESCRIPTION: Frees the amoeba.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :         1993    David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
 public  void  terminate_amoeba(
     amoeba_struct  *amoeba )
 {
@@ -154,6 +256,24 @@ public  void  terminate_amoeba(
     FREE( amoeba->sum );
 }
 
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : try_amoeba
+@INPUT      : amoeba
+              sum
+              high
+              fac
+@OUTPUT     : 
+@RETURNS    : value
+@DESCRIPTION: Does a modification to the high vertex of the amoeba and
+              returns the value of the new point.  If the new point is
+              better (smaller value), it replaces the high vertex of the
+              amoeba.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :         1993    David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
 private  Real  try_amoeba(
     amoeba_struct  *amoeba,
     Real           sum[],
@@ -191,11 +311,31 @@ private  Real  try_amoeba(
 
 #define  N_STEPS_NO_IMPROVEMENT  10
 
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : perform_amoeba
+@INPUT      : amoeba
+@OUTPUT     : 
+@RETURNS    : TRUE if numerically significant improvement
+@DESCRIPTION: Performs one iteration of an amoeba, returning true if a
+              numerically significant improvement has been found recently.
+              Even if it returns FALSE, you can keep calling this function,
+              since it may be contracting with no improvement, but will 
+              eventually shrink small enough to get an improvment.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :         1993    David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
 public  BOOLEAN  perform_amoeba(
     amoeba_struct  *amoeba )
 {
     int     i, j, low, high, next_high;
     Real    y_try, y_save;
+    BOOLEAN  improvement_found;
+    Real tol;
+
+    improvement_found = TRUE;
 
     if( amoeba->values[0] > amoeba->values[1] )
     {
@@ -223,12 +363,20 @@ public  BOOLEAN  perform_amoeba(
             next_high = i;
     }
 
-    if( numerically_close( amoeba->values[low], amoeba->values[high],
+/*    if( numerically_close( amoeba->values[low], amoeba->values[high],
                            amoeba->tolerance ) )
-    {
+ */
+
+    tol = 2.0 * ABS(amoeba->values[high]-amoeba->values[low]) /
+      (ABS(amoeba->values[high]) + ABS(amoeba->values[low]));
+
+    if (tol < amoeba->tolerance)
+   {
         ++amoeba->n_steps_no_improvement;
-        if( ++amoeba->n_steps_no_improvement == N_STEPS_NO_IMPROVEMENT )
-            return( FALSE );
+        if( ++amoeba->n_steps_no_improvement == N_STEPS_NO_IMPROVEMENT ) {
+/*	  print ("no improvement %d\n",amoeba->n_steps_no_improvement);*/
+	  return( FALSE );
+	}
     }
     else
         amoeba->n_steps_no_improvement = 0;
@@ -268,5 +416,7 @@ public  BOOLEAN  perform_amoeba(
         }
     }
 
-    return( TRUE );
+    return( improvement_found );
 }
+
+
