39360c03d9fe6bbbca0f89cf15994aef499254fd
stever <stever>
2000-02-20 04:16:04 +0000

    forgotten files.

---
+++ Proglib/history.c
@@ -0,0 +1,25 @@
+#include <config.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "time_stamp.h"
+
+char* history_string( int ac, char* av[] )
+{
+    char* stamp = time_stamp( ac, av );
+    int len = strlen(stamp) + strlen(MNI_AUTOREG_LONG_VERSION) + 4;
+    char* s = malloc(len);
+
+    if ( s == NULL )
+	print_error_and_line_num( "cannot malloc(%d)", __FILE__, __LINE__, len );
+
+    s[0] = 0;
+    strcat(s, stamp );
+    free( stamp );
+
+    strcat(s, "(" );
+    strcat(s, MNI_AUTOREG_LONG_VERSION);
+    strcat(s, ")\n" );
+    
+    return s;
+}
---
+++ Proglib/history.h
@@ -0,0 +1,7 @@
+
+/*
+ * Generate a history string consisting of the output of time_stamp(),
+ * (which ends with a newline) followed by "(MNI_AUTOREG_LONG_VERSION)\n".
+ */
+char* history_string( int ac, char* av[] );
+
