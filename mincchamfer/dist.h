fe86a12ad7f36fab92e5bc98655df9d2652c765b
stever <stever>
2000-02-29 23:26:32 +0000

    removal of unused files

--- mincchamfer/dist.h
+++
@@ -1,8 +0,0 @@
-/* ----------------------------- MNI Header -----------------------------------
-@NAME       : dist.h
-@DESCRIPTION: prototypes for dist.c
-@CREATED    : Nov 2, 1998 - louis
-@MODIFIED   : 
----------------------------------------------------------------------------- */
-
-public Status compute_dist(Volume  dist);
