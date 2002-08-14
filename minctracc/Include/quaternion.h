8e1db0c2bebf4b231962dd94ee9f1005d21bd6f0
lenezet <lenezet>
2002-08-14 20:20:45 +0000

    I have forgotten to add this file

---
+++ minctracc/Include/quaternion.h
@@ -0,0 +1,39 @@
+void vcopy(double *copy, double *v);
+
+
+void vadd(double *add, double *v1, double *v2);
+
+
+void vscale(double *v, double scale);
+
+
+void vcross(double *cross, double *v1, double *v2);
+
+
+double vdot(double *v1, double *v2);
+
+
+double vlength(double *v);
+
+
+double veuc(double *p1, double *p2);
+
+void vnormal(double *v);
+
+
+void axis_to_quat(double vec[3], double phi, double quat[4]);
+
+
+void quat_to_axis(double vec[3], double *phi, double quat[4]);
+
+
+static void normalize_quat(double q[4]);
+
+void add_quats(double q1[4], double q2[4], double dest[4]);
+
+void build_rotmatrix(float **m, double *quat);
+
+void extract_quaternions(float **m, double *quat);
+
+
+
