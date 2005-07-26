f7b48fb617834b75e664e673eb5c17f3a6941f00
Andrew Janke <a.janke@gmail.com>
2005-07-26 20:24:52 +0000

    * moved MNI_AUTOREG_LONG_VERSION to config.h (via configure.ac)

--- Proglib/Proglib.h
+++ Proglib/Proglib.h
@@ -9,17 +9,8 @@
 /* a few macros for historical reasons */
 #define VOXEL_DATA(vol) ((vol)->array.data)
 
-#define MNI_AUTOREG_LONG_VERSION \
-   "Package " PACKAGE_STRING \
-   ", compiled by " MNI_AUTOREG_COMPILE_USER \
-   "@" MNI_AUTOREG_COMPILE_HOST \
-   " (" MNI_AUTOREG_COMPILE_SYSTEM ")" \
-   " on " MNI_AUTOREG_COMPILE_DATETIME
-
-
 void  print_error_and_line_num( char format[], char *name, int line, ... );
 void  print_version_info( char *version_string);
-
 
 /*
  *  * Generate a history string consisting of the output of time_stamp(),
