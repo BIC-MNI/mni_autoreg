c5d1cbc72c0dc59e38eb3ad8945628bedbcb72d4
Robert Brown <robert.brown@mcgill.ca>
2013-03-14 15:39:38 -0400

    Updated header file for settable iterations

--- minctracc/Include/libminctracc.h
+++ minctracc/Include/libminctracc.h
@@ -2,5 +2,5 @@
 #include "arg_data.h"
 
 void initializeArgs(Arg_Data *args);
-VIO_General_transform* minctracc( VIO_Volume source, VIO_Volume target, VIO_Volume sourceMask, VIO_Volume targetMask, VIO_General_transform *initialXFM, Arg_Data *args);
+VIO_General_transform* minctracc( VIO_Volume source, VIO_Volume target, VIO_Volume sourceMask, VIO_Volume targetMask, VIO_General_transform *initialXFM, int iterations, Arg_Data *args);
 
