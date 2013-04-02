11be35417bcf8147a7df974822020997b5883aeb
Robert Brown <robert.brown@mcgill.ca>
2013-04-02 18:31:29 -0400

    inserted semicolon

--- minctracc/Include/libminctracc.h
+++ minctracc/Include/libminctracc.h
@@ -2,4 +2,4 @@
 #include "arg_data.h"
 
 void initializeArgs(Arg_Data *args);
-VIO_General_transform* minctracc( VIO_Volume source, VIO_Volume target, VIO_Volume sourceMask, VIO_Volume targetMask, VIO_General_transform *initialXFM, int iterations, float weight, float stiffness, float similarity, float sub_lattice, Arg_Data *args)
+VIO_General_transform* minctracc( VIO_Volume source, VIO_Volume target, VIO_Volume sourceMask, VIO_Volume targetMask, VIO_General_transform *initialXFM, int iterations, float weight, float stiffness, float similarity, float sub_lattice, Arg_Data *args);
