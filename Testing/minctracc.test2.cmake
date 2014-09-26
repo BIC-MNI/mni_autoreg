9d7cee84f81c3c32010616853558f650e772a1f2
Robert D. Vincent <robert.d.vincent@mcgill.ca>
2014-09-26 16:36:53 -0400

    Relax test slightly.

--- Testing/minctracc.test2.cmake
+++ Testing/minctracc.test2.cmake
@@ -19,8 +19,8 @@ minctracc -identity object1_dxyz.mnc object2_dxyz.mnc \
 mincresample object1.mnc -like object2.mnc -transform def.xfm  object1_res.mnc -clob
 corr_after=`xcorr_vol object1_res.mnc object2.mnc|cut -c 1-7`
 echo $0 xcorr after\: $corr_after
-
-if [ $corr_after != 0.9860 ];then
+tresult=$(echo "$corr_after>=0.9860 && $corr_after<1.0" | bc)
+if [ $tresult != 1 ];then
   echo $0 Corr test after failed 
   exit 1
 fi
